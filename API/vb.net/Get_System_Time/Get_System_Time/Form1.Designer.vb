ef898091a8d79c8dd24f9dcda74c2770f1c32d59
a5768549 <44581221+a5768549@users.noreply.github.com>
2019-11-08 11:44:55 +0800

    5

---
+++ API/vb.net/Get_System_Time/Get_System_Time/Form1.Designer.vb
@@ -0,0 +1,58 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("新細明體", 16.0!)
+        Me.TextBox1.Location = New System.Drawing.Point(152, 114)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(360, 81)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ API/vb.net/Get_System_Time/Get_System_Time/Form1.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class form1
+    <StructLayout(LayoutKind.Sequential)>
+    Private Structure SYSTEMTIME
+        <MarshalAs(UnmanagedType.U2)> Public Year As Short
+        <MarshalAs(UnmanagedType.U2)> Public Month As Short
+        <MarshalAs(UnmanagedType.U2)> Public DayOfWeek As Short
+        <MarshalAs(UnmanagedType.U2)> Public Day As Short
+        <MarshalAs(UnmanagedType.U2)> Public Hour As Short
+        <MarshalAs(UnmanagedType.U2)> Public Minute As Short
+        <MarshalAs(UnmanagedType.U2)> Public Second As Short
+        <MarshalAs(UnmanagedType.U2)> Public Milliseconds As Short
+    End Structure
+
+    <DllImport("kernel32.dll")>
+    Private Shared Sub GetLocalTime(ByRef time As SYSTEMTIME)
+    End Sub
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim Year, Month, Day, Hour, Minute, Second As String
+        Dim Time As String
+        Dim st As SYSTEMTIME = New SYSTEMTIME()
+        GetLocalTime(st)
+        Year = st.Year
+        Month = st.Month
+        Day = st.Day
+        Hour = st.Hour
+        Minute = st.Minute
+        Second = st.Second
+        Time = Year & "-" & Month & "-" & Day & " " & Hour & ":" & Minute & ":" & Second
+        TextBox1.Text = Time
+    End Sub
+End Class
